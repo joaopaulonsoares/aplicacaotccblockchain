5b01136390c0bf1c94d9c67ca9f0741c34de2f9d
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-08-31 22:44:32 -0300

    Adicionando configuraçoes para utilizar o ReactJS integrado com metamask e blockchain

--- aplicacao_tcc/src/App.js
+++ aplicacao_tcc/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
+import Component from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3'
+import {APP_ABI, APP_ADDRESS} from './config'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      account:''
+    }
+  }
+
+  componentWillMount(){
+    this.loadBlockChainData()
+  }
+
+  async loadBlockChainData(){
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType();
+    //Fetch account
+    const accounts = await web3.eth.getAccounts();
+    this.setState({account:accounts[0]})
+
+    //Instancia o contrato
+    const trafficApp = new web3.eth.Contract(APP_ABI,APP_ADDRESS)
+    this.setState({trafficApp})
+    console.log("contrato "+ trafficApp)
+
+    const ticketsCount = await trafficApp.methods.ticketsCount().call()
+    console.log(ticketsCount)
+
+
+    
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello</h1>
+        <h1>Sua conta é: {this.state.account}</h1>
+      </div>
+    )
+  }
 }
+
 
 export default App;
