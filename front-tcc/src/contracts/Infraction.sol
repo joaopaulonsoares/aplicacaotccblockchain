c9cce5641b9d61d2f7f923137c23b049a02462a1
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-11-08 16:13:51 -0300

    Adicionando nova versao do contrato

--- front-tcc/src/contracts/Infraction.sol
+++ front-tcc/src/contracts/Infraction.sol
@@ -9,6 +9,7 @@ contract InfractionTcc{
     uint public ticketsCount;
     uint public driversCount;
     uint public authoritiesCount;
+    uint public transferTicketsCount;
 
 
     //Model a trafic ticket
@@ -19,11 +20,12 @@ contract InfractionTcc{
         string dateInfraction;
         uint infractionPoints;
         string observations;
-        uint valueToPay; // Value in reais
-        uint statusOfInfraction; // Active, Payed, Canceled, Transfered
+        uint valueToPay; // Value in ethers
+        string statusOfInfraction; // Active, Payed, Canceled, Transfered
+        bool payed;
 
         address infractorDriverAddress;
-        address authorityResponsableAdress;//Owner
+        address payable authorityResponsableAdress; // Authoritie that registered the infraction(will receive the payement of the debt)
     }
 
     // Model a Driver
@@ -32,7 +34,6 @@ contract InfractionTcc{
         string name;
         uint numberOfPoints;
         address driverAddress;
-
     }
 
     // Model Authoritie
@@ -42,23 +43,26 @@ contract InfractionTcc{
         string sigla;
         address authoritieAddress;
     }
+    
+    struct TransferTicket{
+        uint id;
+        uint ticketId;
+        uint status; // 0 -> Pending, 1 -> Accepted, 2 -> Rejected
+        address currentOwnerAddress;
+        address requestedInfractorAddress;
+    }
 
 
     // Map Tickets
     mapping(uint => TraficTicket) public tickets;
-    mapping(address => mapping(uint256=>TraficTicket)) public driveTickets; // we shall have the tickets mapped against each address to recover all drivers tickets
-    mapping(address => uint256) individualCountOfTraficTicket;			// how many tickets does a particular person hold
-
-    
-    mapping(uint => Drivers) public drivers; // Para simular o banco auxiliar de motoristas
-    mapping(address => Drivers) public driversListAddress;
-    
-    mapping(address => Authorities) public authoritiesMap;
+    mapping(uint => Drivers) public drivers;
+    mapping (address => Drivers) driversListAddress; // Just to get driver Id easily 
     mapping(uint => Authorities) public authorities; // Para simular o banco auxiliar de autoridades
+    mapping(uint => TransferTicket) public tranferTicketsRequests;
 
 
 
-    // Create Lists
+    // Create Lists to access vlues
     uint[] public ticketList;
     address[] public driversList;
     uint[] public driverListUint;
@@ -84,6 +88,17 @@ contract InfractionTcc{
     event registeredAuthoritieEvent (
         address authoritieAddress
     );
+    
+    event TicketPayedEvent (
+        uint ticketId,
+        address accountThatPayed,
+        string message
+    );
+    
+    event TransferTicketEvent(
+        string message
+    );
+    
 
 
     //  ============================================================== CONSTRUCTOR ==============================================================
@@ -121,7 +136,7 @@ contract InfractionTcc{
 
     function registerAuthoritie(string memory _name,string memory _sigla, address _authoritieAddress) public{
 
-        authoritiesMap[_authoritieAddress] = Authorities(authoritiesCount, _name, _sigla, _authoritieAddress);
+        //authoritiesMap[_authoritieAddress] = Authorities(authoritiesCount, _name, _sigla, _authoritieAddress);
         authorities[authoritiesCount] = Authorities(authoritiesCount, _name, _sigla, _authoritieAddress);
 
         authoritiesCount++;
@@ -131,9 +146,9 @@ contract InfractionTcc{
 
     function registerDriver(string memory _name,uint numberOfPoints, address _driverAddress) public{
 
-        driversListAddress[_driverAddress] = Drivers(driversCount, _name, numberOfPoints, _driverAddress);
+        //driversListAddress[_driverAddress] = Drivers(driversCount, _name, numberOfPoints, _driverAddress);
         drivers[driversCount] = Drivers(driversCount, _name, numberOfPoints, _driverAddress);
-
+        driversListAddress[_driverAddress] =  Drivers(driversCount, _name, numberOfPoints, _driverAddress);
         driversCount++;
         emit registeredDriverEvent(msg.sender);
     }
@@ -142,30 +157,55 @@ contract InfractionTcc{
     function registerInfraction(string memory _vehiclePlate, uint _infractionCategory,
                                 string memory _dateInfraction, uint _infractionPoints,
                                 string memory _observations, uint _valueToPay,
-                                uint _statusOfInfraction, address _infractorDriverAddress
-    )public{
+                                string memory _statusOfInfraction, address _infractorDriverAddress, uint _infractorDriverId)public{
         // Require that the account registering the infraction is a authoritie
         //require(authorities[msg.sender]) = true;
+        Drivers memory _driver = drivers[_infractorDriverId];
+
 
         tickets[ticketsCount] = TraficTicket(ticketsCount, _vehiclePlate, _infractionCategory, _dateInfraction, _infractionPoints, _observations,
-                                _valueToPay, _statusOfInfraction, _infractorDriverAddress, msg.sender);
+                                _valueToPay, _statusOfInfraction, false,  _infractorDriverAddress, msg.sender);
 
         // Update driver points
-        uint driverPointsAfterInfraction = driversListAddress[_infractorDriverAddress].numberOfPoints + _infractionPoints;
-        driversListAddress[_infractorDriverAddress].numberOfPoints = driverPointsAfterInfraction;
+        uint driverPointsAfterInfraction = drivers[_infractorDriverId].numberOfPoints + _infractionPoints;
+        //driversListAddress[_infractorDriverAddress].numberOfPoints = driverPointsAfterInfraction;
+        _driver.numberOfPoints = driverPointsAfterInfraction;
+        drivers[_infractorDriverId] = _driver;
+        
         
 
         // trigger infraction registered event
         emit registeredInfraction( msg.sender, _infractorDriverAddress, "Infracao registrada com sucesso!" );
         ticketsCount++;
     }
+    
 
+        
+    function registerTransferTicketRequest(uint _ticketId, 
+                                           address _currentOwnerAddress, 
+                                           address _requestedInfractorAddress )
+    public{
+         // Get Ticket INFORMATION
+        TraficTicket memory _ticket = tickets[_ticketId];
+        
+        // Checks if is the current owner of infraction that is requesting
+        require(_ticket.infractorDriverAddress == msg.sender);
+        
+        tranferTicketsRequests[transferTicketsCount] = TransferTicket(transferTicketsCount, _ticketId, 0,
+                                                                      msg.sender, _requestedInfractorAddress);
+        transferTicketsCount++;
+        
+        emit TransferTicketEvent("Requisicao para transferencia de infracao realizada com sucesso!");
+                                                                      
+    }
+    
+     
     //  ============================================================== GET INFORMATION FUNCTIONS ==============================================================
 
     function getTicketInformationById(uint ticket_id) public view returns(string memory vehiclePlate, uint infractionCategory,
                                 string memory dateInfraction, uint infractionPoints,
                                 string memory observations, uint valueToPay,
-                                uint statusOfInfraction, address infractorDriverAddress, address authorityResponsableAdress
+                                string memory statusOfInfraction, address infractorDriverAddress, address authorityResponsableAdress
     ){
         vehiclePlate = tickets[ticket_id].vehiclePlate;
         infractionCategory = tickets[ticket_id].infractionCategory;
@@ -180,11 +220,11 @@ contract InfractionTcc{
 
 
 
-    function getDriverInformationByAddress(address driverAddress) public view returns (string memory name,uint numberOfPoints){
+  /*  function getDriverInformationByAddress(address driverAddress) public view returns (string memory name,uint numberOfPoints){
         name = driversListAddress[driverAddress].name;
         numberOfPoints = driversListAddress[driverAddress].numberOfPoints;
 
-    }
+    }*/
 
 
     function getDriverInformationById(uint driverId) public view returns (string memory name,uint numberOfPoints, address driverAddress){
@@ -206,6 +246,132 @@ contract InfractionTcc{
         authoritieAddress = authorities[authoritieId].authoritieAddress;
     }
     
+    function getDriverIdByAddress(address driverAddress) private view returns (uint id){
+        id = driversListAddress[driverAddress].id;
+    }
+    
+    // ============================================== TRANSFER INFRACTION FUNCTIONS ==================================================
+    
+    function acceptTransferTicketRequest(uint _tranferTicketRequestId)
+    public{
+        
+        
+         // Get Transfer request informations
+        TransferTicket memory _transferTicketRequest = tranferTicketsRequests[_tranferTicketRequestId];
+        
+        // Checks if is the status is Pending 
+        require(_transferTicketRequest.status == 0);
+        
+        // Checks if is the requested owner of infraction that is requesting
+        require(_transferTicketRequest.requestedInfractorAddress == msg.sender);
+        
+        // Update request status
+        _transferTicketRequest.status = 1; //Accepted status
+        tranferTicketsRequests[_tranferTicketRequestId] = _transferTicketRequest;
+        
+        // Load Ticket and update current owner information
+        TraficTicket memory _ticket = tickets[_transferTicketRequest.ticketId];
+        uint oldOwnerId = getDriverIdByAddress(_ticket.infractorDriverAddress);
+        
+        _ticket.infractorDriverAddress = msg.sender;
+        tickets[_transferTicketRequest.ticketId] = _ticket;
+        
+        // Remove points from old owner and add to current owner
+        uint newOwnerId = getDriverIdByAddress(msg.sender);
+        
+        // Update old driver points
+        Drivers memory _oldTicketOwnerDriver = drivers[oldOwnerId];
+        uint oldDriverPointsAfterInfraction = drivers[oldOwnerId].numberOfPoints - _ticket.infractionPoints;
+        _oldTicketOwnerDriver.numberOfPoints = oldDriverPointsAfterInfraction;
+        drivers[oldOwnerId] = _oldTicketOwnerDriver;
+        
+        
+        // Update new driver points
+        Drivers memory _newTicketOwnerDriver = drivers[newOwnerId];
+        uint newDriverPointsAfterInfraction = drivers[newOwnerId].numberOfPoints + _ticket.infractionPoints;
+        _newTicketOwnerDriver.numberOfPoints = newDriverPointsAfterInfraction;
+        drivers[newOwnerId] = _newTicketOwnerDriver;
+        
 
+        emit TransferTicketEvent("Requisicao para transferencia de infracao aceitada com sucesso!");
+                                                                      
+    }
+    
+    function rejectTransferTicketRequest(uint _tranferTicketRequestId)
+    public{
+         // Get Transfer request informations
+        TransferTicket memory _transferTicketRequest = tranferTicketsRequests[_tranferTicketRequestId];
+        
+        // Checks if is the status is Pending 
+        require(_transferTicketRequest.status == 0);
+        
+        // Checks if is the requested owner of infraction that is requesting
+        require(_transferTicketRequest.requestedInfractorAddress == msg.sender);
+        
+        // Update request status
+        _transferTicketRequest.status = 2; //Rejected status
+        tranferTicketsRequests[_tranferTicketRequestId] = _transferTicketRequest;
+        
+
+        emit TransferTicketEvent("Requisicao para transferencia de infracao rejeitada com sucesso!");
+                                                                      
+    }
+    
+    
+    // =========================================================== PAY INFRACTION ==================================================================================
+    
+    function payInfraction(uint _ticketId) public payable {
+        // Get Ticket INFORMATION
+        TraficTicket memory _ticket = tickets[_ticketId];
+        
+        // Get the authoritie account
+        address payable _authoritieReceiver = _ticket.authorityResponsableAdress;
+        
+        // Checks if is not the authoritie trying to pay the account
+        require(_authoritieReceiver != msg.sender);
+        
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _ticket.valueToPay);
+        
+        _ticket.payed = true;
+        _ticket.statusOfInfraction = "Payed";
+        
+        tickets[_ticketId] = _ticket;
+        
+       // address(_authoritieReceiver).transfer(msg.value);
+        
+        emit TicketPayedEvent(_ticketId, msg.sender, "Infracao paga com sucesso!");
+        
+    }
+    
+    // ======================================================== CANCEL INFRACTION ====================================================
+
+    function cancelInfraction(uint _ticketId) public {
+        // Get Ticket INFORMATION
+        TraficTicket memory _ticket = tickets[_ticketId];
+        
+
+        
+        // Checks if is the responsible authoritie
+        require(_ticket.authorityResponsableAdress == msg.sender);
+        
+        
+        _ticket.payed = false;
+        _ticket.statusOfInfraction = "Canceled";
+        
+        tickets[_ticketId] = _ticket;
+        
+        // Remove driver points
+        uint _driverId = getDriverIdByAddress(_ticket.infractorDriverAddress);
+        Drivers memory _driver = drivers[_driverId];
+        uint driverPointsAfterCancel = drivers[_driverId].numberOfPoints - _ticket.infractionPoints;
+        _driver.numberOfPoints = driverPointsAfterCancel;
+        drivers[_driverId] = _driver;
+        
+        
+        
+        //emit TicketPayed(_ticketId, msg.sender, "Infracao cancelada com sucesso!");
+        
+    }
 
 }
